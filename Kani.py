03e60323509592df17dc10fa22e6ea881a7d64ff
Kanex881 <kanikesh.iyer@gmail.com>
2025-06-02 10:40:45 +0530

    Add files via upload

---
+++ Kani.py
@@ -0,0 +1,116 @@
+# print("hello World")
+# print("Welcome to Python Intro")
+# print("I am the best.\n I am really the best.")
+# '''
+# Syntax : rules of the template of a programming language
+# if there is any mistake in the code, you will get a syntax error
+#
+# '''
+# print("Name:John Doe.\nAddress: 123 Main St")
+# print("                                                ")
+# print("Item     price\n-----     -----\nApple     $1.00")
+# print("Hello\tWorld" )
+# print("Item \t \t price \n----- \t\t-----\napple\t\t$1.00\n----- \t\t-----\nBanana\t\t$2.00\n----- \t\t-----\nGrapes\t\t$5.00")
+#  a=200
+#  a=100000000
+#  a=12343210
+#  print(a)
+# class=100
+# print(class)
+# Data1 = 1500
+# Data2 = 100
+# print("The Data 1 is",Data1,"The Data 2 is",Data2 )
+# print(f"The Data 1 is, {Data1},The Data 2 is {Data2}")
+# var1 = 1577
+# var2 = 0.14357
+# var3 = 0.00000001
+# var4 = "My Name Is Kanikesh"
+# var5 = "I am 15 years old"
+# var6 = "What is your name?"
+#
+# print(var1)
+# print(var2)
+# print(var3)
+# print(var4)
+# print(var5)
+# var = 150
+# dt = type(var)
+# print(dt)
+# num1=10000
+# num2=10000
+# print(f"The floor Divided answer of {num1} and {num2} is {num1//num2}")
+# print(f"The division answer of {num1} and {num2} is {num1/num2}")
+# print("The remainder value of {num1} divided by {num2}is")
+# print(f"The exponential form of {num1} to the power {num2} is {num1**num2}")
+# a=int(input("Enter Birth year"))
+# b=int(input("Enter Current year"))
+# b=int(input("Enter Base Length"))
+# print("The formula for the triangle is 0.5 multiplied by height multiplied by base.")
+# print("The area of the triangle with provided side length and Base is",0.5*a*b)
+# print("The formula for Circle is pi multiplied by r^2.")
+# print("The area of the circle of given radius is,",3.14*a*a)
+# a=int(a)
+# print(type(a))
+# print("Your current age is",b-a)
+# comparison operators:Use dot compare values
+#The result of comparison is Boolean Value
+# a=149
+# b=149
+# print(a==b)
+# print(a!=b)
+# print(a>b)
+# print(a<b)
+# print(a>=b)
+# print(a<=b)
+
+
+# a=int(input("Enter new number 1: "))
+# b=int(input("Enter new number 2: "))
+# operator=input("Enter Operator Symbol : ")
+#
+# if operator == "+":
+#     ans=a+ b
+#     print(f"The addition of {a} and {b} is {ans}")
+#
+#
+# elif operator == "-":
+#     ans=a-b
+#     print(f"The difference of {a} and {b} is {ans}")
+#
+#
+# elif operator == "*":
+#     ans=a*b
+#     print(f"The difference of {a} and {b} is {ans}")
+#
+# elif operator == "**":
+#     ans=a**b
+#     print(f"The difference of {a} and {b} is {ans}")
+#
+# elif operator == "/":
+#     ans=a/b
+#     print(f"The difference of {a} and {b} is {ans}")
+
+#Iterating variable can have any name.
+#Keeps track of how many times the loop is running
+#a=0
+#a=1
+#a=2
+#a=3
+#a=4
+#a=5
+#a=6
+#a=7
+#a=8
+#a=9
+# a=input("Enter Anything")
+# for a in range (100):
+# for a in range (1,51):
+#     print(a)
+#
+# for b in range (50,101):
+#     print(b)
+for i in range (10,0,-1):
+    print(i)
+
+for q in range (55,-11,-5):
+    print(q)
